8ef80cb4148abfd106f6bffabe77cb14295ad712
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-09-13 08:08:55 -0400

    retomando as atividades de python

--- Exercícios/ex042.py
+++ Exercícios/ex042.py
@@ -1,10 +1,11 @@
 p1 = int(input('Primeiro segmento: '))
 p2 = int(input('Segundo segmento: '))
 p3 = int(input('Terceiro segmento: '))
-eq = p1 == p2 and p3
-iso = p1 == p2 or p1 == p3
-es =  p1 != p2 and p3
+eq = p1 == p2 == p3
+es =  p1 != p2 != p3 != p1
 if eq:
+    print('Os segmentos acima PODEM FORMAR um triângulo EQUILÁTERO!')
+elif es:
     print('Os segmentos acima PODEM FORMAR um triângulo ESCALENO!')
-elif iso:
-    print('Os segmentos acima PODEM FORMAR um triângulo ESCALENO!')')
+else:
+    print('Os segmentos acima PODEM FORMAR um triângulo ISÓSCELES')
