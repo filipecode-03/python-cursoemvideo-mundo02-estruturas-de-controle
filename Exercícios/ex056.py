71ac48f7feca5505c614784933b54421c568b13a
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-09-16 14:09:49 -0400

    retomando as atividades

--- Exercícios/ex056.py
+++ Exercícios/ex056.py
@@ -1,6 +1,7 @@
 sidade = 0
 maior = 0
 menor = 0
+nomemaioridade = ''
 for c in range(1, 5):
     print(f'----- {c}ª PESSOA -----')
     nome = str(input('Nome: '))
@@ -14,6 +15,7 @@ for c in range(1, 5):
     else:
         if idade > maior:
             maior = idade
+            nomemaioridade = nome
         if idade < menor:
             menor = idade
 print(f'A média de idade do grupo é de {media:.1f} anos')
