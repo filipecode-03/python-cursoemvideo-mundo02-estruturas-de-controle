d2847bed2bf1d8b0fbdbde1d2e29a45ce32f82b9
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-09-16 14:39:52 -0400

    terminei os exercicios da aula 13

--- Exercícios/ex056.py
+++ Exercícios/ex056.py
@@ -1,23 +1,20 @@
 sidade = 0
-maior = 0
-menor = 0
-nomemaioridade = ''
+nome_maior_idade = ''
+maior_idade = 0
+totalf = 0
 for c in range(1, 5):
     print(f'----- {c}ª PESSOA -----')
     nome = str(input('Nome: '))
     idade = int(input('Idade: '))
-    sexo = str(input('Sexo [M/F]: '))
+    sexo = str(input('Sexo [M/F]: ')).upper
     sidade = sidade + idade
-    media = sidade / 4
-    if c == 'M':
-        maior = idade
-        menor = idade
-    else:
-        if idade > maior:
-            maior = idade
-            nomemaioridade = nome
-        if idade < menor:
-            menor = idade
+    media = sidade / c
+    if sexo == 'M':
+      if idade > maior_idade:
+         maior_idade = idade
+         nome_maior_idade = nome
+    if sexo == 'F' and idade < 20:
+        totalf = totalf + 1
 print(f'A média de idade do grupo é de {media:.1f} anos')
-#print('O homem mais velho tem {} anos e se chama {}')
-#print('Ao todo são {} mulheres com menos de {} anos')
+print(f'O homem mais velho tem {maior_idade} anos e se chama {nome_maior_idade}')
+print(f'Ao todo são {totalf} mulheres com menos de 20 anos')
